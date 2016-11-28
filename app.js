9dd7c6b484fed44da8ebc483e2f36a4592d7defb
rafanog2155 <rafanog2155@gmail.com>
2016-11-28 11:53:34 -0400

    Added bower.json file

---
+++ app.js
@@ -0,0 +1,10 @@
+var salut = 'Hello world from app.js'
+document.write(salut);
+
+var hello = function(){
+	console.log(salut+' from a button');
+}
+
+var hello_alert = function(){
+	alert(salut + ' from the alert function');
+}
